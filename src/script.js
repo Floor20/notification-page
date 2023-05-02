31898c5b2680aa91648379909f1353a92dbe8828
Floor20 <austino2112@gmail.com>
2023-05-02 14:12:35 -0600

    Completed JS behavior

--- src/script.js
+++ src/script.js
@@ -2,15 +2,27 @@
 
 let notificationNumber = document.querySelector(".notif-number");
 const notificationTiles = document.querySelectorAll(".notif-tile");
-const notificationDesc = document.querySelector(".notif-description");
 const redDot = document.querySelector(".red-dot");
+const markReadButton = document.querySelector(".mark-btn");
 
 notificationNumber.textContent = notificationTiles.length;
 
-notificationTiles.forEach((tile) => {
-  tile.addEventListener("click", function () {
-    this.style.backgroundColor = "hsl(0, 0%, 100%)";
-    this.getElementsByClassName("red-dot")[0].classList.add("hidden");
-    notificationNumber.textContent--;
+const markAsRead = function () {
+  this.style.backgroundColor = "hsl(0, 0%, 100%)";
+  this.getElementsByClassName("red-dot")[0].classList.add("hidden");
+  if (notificationNumber.textContent > 0) notificationNumber.textContent--;
+};
+
+const markAllAsRead = function () {
+  notificationTiles.forEach((tile) => {
+    if (tile.style.backgroundColor !== "hsl(0, 0%, 100%)")
+      markAsRead.call(tile);
   });
+};
+
+// EVENT HANDLERS
+notificationTiles.forEach((tile) => {
+  tile.addEventListener("click", markAsRead.bind(tile));
 });
+
+markReadButton.addEventListener("click", markAllAsRead);
